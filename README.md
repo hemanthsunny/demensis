f2890ea98a389106163f1331b4a34e2b22b86f70
nagaperla <60889195+nagaperla@users.noreply.github.com>
2021-02-23 16:06:12 +0000

    Testing deploy build

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project_AD
-Master's project
+Master's project.
+1. Install all dependencies: `npm install`
+2. Start server: `npm start`
--- src/components/auth/logout/component.js
+++ src/components/auth/logout/component.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import HeaderComponent from "./header";
 import { signout } from "../../../config/firebase";
 
 function LogoutComponent() {
@@ -13,8 +14,10 @@ function LogoutComponent() {
 
   return (
     <div>
-      LogoutComponent
-      <button onClick={logout}>Logout</button>
+      <HeaderComponent />
+      <div className="login-page-wrapper">
+        <button className="btn btn-outline-primary" onClick={logout}>Logout</button>
+      </div>
     </div>
   )
 }
