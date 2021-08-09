c8a651d9d20f2ac1fe3084bcaaab540c91d3e676
naga_perla <naga@oslr.co.uk>
2021-08-09 01:38:13 +0100

    Removed extra columns and changed report name

--- src/components/dashboard/results/component.js
+++ src/components/dashboard/results/component.js
@@ -41,12 +41,12 @@ function ChildComponent({ currentUser }) {
     });
     doc.autoTable(["Key", "Value"], tableRows)
     // doc.text("Demented patient details", 14, 15)
-    doc.save(`report_${record.createdAt}.pdf`)
+    doc.save(`demensis_report_${record.createdAt}.pdf`)
   }
 
   return (
     <div className="result-wrapper">
-      <table className="table">
+      <table className="table table-responsive text-center">
         <thead>
           <tr>
             <td>Status</td>
@@ -55,8 +55,6 @@ function ChildComponent({ currentUser }) {
             <td>Created at</td>
             <td>Result</td>
             <td>Accuracy</td>
-            <td>Precision</td>
-            <td>Significant difference</td>
             <td>Download</td>
             <td>Age</td>
             <td>Gender</td>
@@ -74,8 +72,6 @@ function ChildComponent({ currentUser }) {
               <td>{moment(res.createdAt).format("YYYY/MM/DD HH:mm:ss")}</td>
               <td className="text-capitalize">{res.result}</td>
               <td>{Math.round(res.accuracy)}%</td>
-              <td>{res.precision}</td>
-              <td>{res.significant_difference}</td>
               <td>
                 <button className="btn btn-outline-primary btn-sm" onClick={e => downloadPdf(res)}>
                   <i className="fa fa-download"></i> Download
